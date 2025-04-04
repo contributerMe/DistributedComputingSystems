7f8134c7543a3f332cdd30ab3b012ada103597d1
Nimesh <130450352+contributerMe@users.noreply.github.com>
2025-04-04 12:05:35 +0530

    Delete Ricard directory

--- Ricard/v1 - Copy/ProcessState.java
+++
@@ -1,3 +0,0 @@
-public enum ProcessState {
-  RELEASED, WANTED, HELD
-}
--- Ricard/v1 - Copy/test.java
+++
@@ -1,6 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Java compiler is working correctly!");
-        System.out.println("Java version: " + System.getProperty("java.version"));
-    }
-}
